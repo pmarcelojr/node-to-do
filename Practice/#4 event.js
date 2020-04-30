5e78479a990317d9a43317eeaee0f163a4f18b7b
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-04-30 16:09:45 -0300

    created event Emitter

--- Practice/#4 event.js
+++ Practice/#4 event.js
@@ -1,9 +1,22 @@
 const EventEmitter = require('events')
+const util = require('util')
 
-const myEmitter = new EventEmitter()
+let Person = function(name){
+    this.name = name
+}
 
-myEmitter.on('click', (msg)=>{
-    console.log(msg)
+util.inherits(Person, EventEmitter)
+
+let james = new Person('James')
+let mary = new Person('Mary')
+let ryu = new Person('Ryu')
+let people = [james, mary, ryu]
+
+people.forEach(function(person){
+    person.on('speak', function(msg){
+        console.log(`${person.name}, said ${msg}`)
+    })
 })
 
-myEmitter.emit('click', "the event was emitted")
+james.emit('speak', "hey dudes")
+ryu.emit('speak', 'I want a currys')
