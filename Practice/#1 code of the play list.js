988334aba6a79e5cc78bc12201336487dc5fb137
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-04-30 15:21:47 -0300

    first exercise

---
+++ Practice/#1 code of the play list.js
@@ -0,0 +1,13 @@
+let time = 0
+
+let timer = setInterval(() => {
+    time += 2
+    console.log(`${time} seconds have passed`)
+    if(time > 5){
+        clearInterval(timer)
+    }
+}, 2000);
+
+
+console.log(__dirname)
+console.log(__filename)
