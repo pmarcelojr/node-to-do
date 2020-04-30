b096527a89b60fd10832d6d29ca10cf6b773551a
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-04-30 15:48:08 -0300

    creating module

---
+++ Practice/#6 moduleexp.js
@@ -0,0 +1,10 @@
+let counter = function(arr) {
+    return `There are ${arr.length} elements in this array`
+}
+
+let adder = function(a,b){
+    return `The sum of the 2 numbers is ${a+b}`
+}
+
+module.exports.counter = counter
+module.exports.adder = adder
