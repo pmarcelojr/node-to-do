7d9af48592e35f78c819f8d7e327c6b4ed11cf17
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-05-01 01:38:03 -0300

    working with template engine ejs second part

--- Practice/#12 express introducion/app.js
+++ Practice/#12 express introducion/app.js
@@ -14,7 +14,7 @@ app.get('/contact', (req, res) => {
 })
 
 app.get('/profile/:name', (req, res) => {
-    let data = {age: 26, job: 'ninja'}
+    let data = {age: 26, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing']}
     res.render('profile', {person: req.params.name, data: data})
 })
 
